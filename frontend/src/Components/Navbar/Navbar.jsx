14377a0d24b749b28ec8bb1823e75bab994c7ff1
KevinRichard1 <kevin.rohit@gmail.com>
2024-10-19 20:06:19 -0400

    Updated home page to display collections and offers

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import {NavLink } from 'react-router-dom'
 
 const Navbar = () => {
 
@@ -14,14 +15,14 @@ const Navbar = () => {
             <p>SHOPPER</p>
         </div>
         <ul className="nav-menu">
-            <li onClick={()=>{setMenu("shop")}}>Shop{menu === "shop"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("mens")}}>Men{menu === "mens"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("womens")}}>Women{menu === "womens"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("kids")}}>Kids{menu === "kids"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("shop")}}><NavLink style={{textDecoration: 'none'}} to = '/'>Shop</NavLink>{menu === "shop"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("mens")}}><NavLink style={{textDecoration: 'none'}} to = '/mens'>Men</NavLink>{menu === "mens"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("womens")}}><NavLink style={{textDecoration: 'none'}} to = '/womens'>Women</NavLink>{menu === "womens"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("kids")}}><NavLink style={{textDecoration: 'none'}} to = '/kids'>Kids</NavLink>{menu === "kids"?<hr/>:<></>}</li>
         </ul>
         <div className="nav-login-cart">
-            <button>Login</button>
-            <img src={cart_icon} alt = ""></img>
+            <NavLink to = '/login'><button>Login</button></NavLink>
+            <NavLink to = '/cart'><img src={cart_icon} alt = "" /></NavLink>
             <div className="nav-cart-count">0</div>
         </div>
     </div>
