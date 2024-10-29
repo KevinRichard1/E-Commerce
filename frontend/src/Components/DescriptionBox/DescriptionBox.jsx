e8ecc5585ca176e49a9379abc2dc2a0b38e3ea54
KevinRichard1 <kevin.rohit@gmail.com>
2024-10-29 18:27:29 -0400

    Created description box component for products

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Description of product.</p>
+            <p>More details</p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
