fa1b722ba3d681ddb299b033d2c1b6f107d8ad93
KevinRichard1 <kevin.rohit@gmail.com>
2024-11-30 20:25:07 -0500

    Added responsiveness to home page and navigation bar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,21 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import {NavLink } from 'react-router-dom'
 import { ShopContext } from '../../Context/ShopContext'
+import nav_dropdown from '../Assets/nav_dropdown.png'
 
 const Navbar = () => {
 
     const [menu, setMenu] = useState("shop");
     const {getTotalCartItems}= useContext(ShopContext);
+    const menuRef = useRef();
+
+    const dropdown_toggle = (e) => {
+        menuRef.current.classList.toggle('nav-menu-visible');
+        e.target.classList.toggle('open');
+    }
  
   return (
     <div className = 'navbar'>
@@ -16,7 +23,8 @@ const Navbar = () => {
             <img src={logo} alt = "" />
             <p>SHOPPER</p>
         </div>
-        <ul className="nav-menu">
+        <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
+        <ul ref={menuRef} className="nav-menu">
             <li onClick={()=>{setMenu("shop")}}><NavLink style={{textDecoration: 'none'}} to = '/'>Shop</NavLink>{menu === "shop"?<hr/>:<></>}</li>
             <li onClick={()=>{setMenu("mens")}}><NavLink style={{textDecoration: 'none'}} to = '/mens'>Men</NavLink>{menu === "mens"?<hr/>:<></>}</li>
             <li onClick={()=>{setMenu("womens")}}><NavLink style={{textDecoration: 'none'}} to = '/womens'>Women</NavLink>{menu === "womens"?<hr/>:<></>}</li>
